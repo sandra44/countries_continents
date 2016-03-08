764f517c65e49a0cfec30d0dbb2cee8319a7256c
saijal <saijal1295@gmail.com>
2016-03-08 21:02:07 +0530

    Create countries_func.py

---
+++ countries_func.py
@@ -0,0 +1,18 @@
+def create_country_nodes(list_of_countries):
+	dummylist1=[]
+    for countries in list_of_countries:
+        dummylist1.append(Node("COUNTRY",country_name=countries)) 
+	return none
+
+def create_city_nodes(list_of_cities):
+	dummylist2=[]
+    for cities in list_of_cities:
+        dummylist2.append(Node("CITY",city_name=cities)) 
+	return none
+
+def create_continent_nodes(list_of_continents):
+	dummylist3=[]
+    for continents in list_of_continents:
+        dummylist3.append(Node("CONTINENT",continent_name=continents))   	
+
+	return none

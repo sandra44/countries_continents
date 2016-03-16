a340691a23078f6aafe7efd7b6abe88c96b7758d
punarvasu510 <punarvasu510@gmail.com>
2016-03-16 23:27:57 +0530

    continent-country mapping

--- 3_CCMap.py
+++ 3_CCMap.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python
-import urllib2
+
+import requests
 import json
+url = 'https://restcountries.eu/rest/v1/all'
+response = requests.get(url)
 
+j_obj = json.loads(response.text)
 
-j = urllib2.urlopen('http://restcountries.eu/rest/v1/all')
-j_obj = json.load(j)
-
+#count = 0
 data = {}
-temp = {}
+
 for country in j_obj:
-        if country['region'] in data.keys():
-                temp=data[country['region']]
-                temp[country['name']]=[]
-        else:
-                temp[country['name']]=[]
-                data[country['region']]=temp
 
-final = json.dumps(data)
+	if country['region'] not in data.keys():
+		data[country['region']] = {}
 
-#print final
+	data[country['region']][country['name']]=[]
+		
+	#count = count + 1
+	#if count > 10:
+	#	break
+
+print (data)
 
 f1 = open('final.json', 'w')
-f1.write(final)
+f1.write(json.dumps(data))
 f1.close()
+
